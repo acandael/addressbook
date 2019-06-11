c72a45806f7d5898d181606a3c5a0f9a1279e317
acandael <info@anthonycandaele.com>
2019-06-11 13:57:25 +0200

    setup contacts array

---
+++ js/contacts.js
@@ -0,0 +1,86 @@
+contacts = [
+  {
+    name: "Armand",
+    surname: "Lambert",
+    phone: "06-49-05-58-42",
+    address: "751 rue cyrus-hugues, toulon, lot-et-garonne",
+    image: "../images/armand-lambert.jpg"
+  },
+  {
+    name: "Nathan",
+    surname: "Roberts",
+    phone: "(108)-805-9730",
+    address: "7404 old taupo road, hamilton, waikato",
+    image: "../images/nathan-roberts.jpg"
+  },
+  {
+    name: "Ralph",
+    surname: "King",
+    phone: "0710-783-302",
+    address: "1455 park lane, coventry, south yorkshire",
+    image: "../images/ralph-king.jpg",
+  },
+  {
+    name: "Dominic",
+    surname: "Goertz",
+    phone: "0175-2450017",
+    address: "bahnhofstraße 92, werther (westf.), baden-württemberg",
+    image: "../images/dominic-goertz.jpg"
+  },
+  {
+    name: "Noémie",
+    surname: "Chow",
+    phone: "681-021-3012",
+    address: "2660 oak st, red rock, nova scotia",
+    image: "../images/noemie-chow.jpg"
+  },
+  {
+    name: "Steve",
+    surname: "Martinez",
+    phone: "(949)-181-1517",
+    address: "941 boulevard de la duchère, nebikon, appenzell ausserrhoden",
+    images: "../images/steve-martinez.jpg"
+  },
+  {
+    name: "Thomas",
+    surname: "Fortin",
+    phone: "169-567-8475",
+    address: "9754 richmond ave, bath, new brunswick",
+    images: "../images/thomas-fortin.jpg"
+  },
+  {
+    name: "Ernesto",
+    surname: "Fuentes",
+    phone: "623-630-494",
+    address: "2803 ronda de toledo, vigo, ceuta",
+    images: "../images/ernesto-fuentes.jpg"
+  },
+  {
+    name: "Kerttu",
+    surname: "Jokela",
+    phone: "048-783-25-34",
+    address: "2486 verkatehtaankatu, pedersöre, northern ostrobothnia",
+    images: "../images/kerttu-jokela.jpg"
+  },
+  {
+    name: "Clara",
+    surname: "Lopez",
+    phone: "609-459-960",
+    address: "7112 calle del barquillo, logroño, comunidad de madrid",
+    images: "../images/clara-lopez.jpg"
+  },
+  {
+    name: "Christian",
+    surname: "Rasmussen",
+    phone: "99740353",
+    address: "8777 mirabellevej, sundby/erslev, syddanmark",
+    images: "../christian-rasmussen.jpg"
+  },
+  {
+    name: "Greg",
+    surname: "Richardson",
+    phone: "(975)-587-1307",
+    address: "2375 Airport Ave",
+    images: "../greg-richardson.jpg"
+  }
+];
