caea823c5aef1c47c3c32dfe0d4b8837c342ea11
acandael <info@anthonycandaele.com>
2019-06-12 10:03:24 +0200

    style form

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ const loadGallery = () => {
   <div class="card">
                 <div class="card-img-container">
                     <img class="card-img" src="${
-                      contact.image
-                    }" alt="profile picture">
+      contact.image
+      }" alt="profile picture">
                 </div>
                 <div class="card-info-container">
                     <h3 id="name" class="card-name cap">${contact.name} ${
       contact.surname
-    }</h3>
+      }</h3>
                     <p class="card-text">${contact.phone}</p>
                     <p class="card-text cap">${contact.address}</p>
                 </div>
@@ -29,7 +29,7 @@ const cards = document.getElementsByClassName('card');
 
 // Listen for click event on each card
 for (let i = 0; i < cards.length; i += 1) {
-  cards[i].addEventListener('click', function(e) {
+  cards[i].addEventListener('click', function (e) {
     let modal = '';
     const contact = contacts[i];
 
@@ -40,15 +40,15 @@ for (let i = 0; i < cards.length; i += 1) {
           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
           <div class="modal-info-container">
               <img class="modal-img" src="${
-                contact.image
-              }" alt="profile picture">
+      contact.image
+      }" alt="profile picture">
               <h3 id="name" class="modal-name cap">${contact.name} ${
       contact.surname
-    }</h3>
+      }</h3>
               <hr>
               <p class="modal-text"><a href="tel:${contact.phone}">${
       contact.phone
-    }</a></p>
+      }</a></p>
               <p class="modal-text">${contact.address}</p>
           </div>
         </div>
@@ -64,7 +64,7 @@ for (let i = 0; i < cards.length; i += 1) {
     // Close Modal
     const closeButton = document.getElementById('modal-close-btn');
 
-    closeButton.addEventListener('click', function(e) {
+    closeButton.addEventListener('click', function (e) {
       this.parentNode.style.display = 'none';
       // remove modal
       document.getElementById('gallery').removeChild(overlay);
@@ -74,24 +74,37 @@ for (let i = 0; i < cards.length; i += 1) {
 
 // Add Contact
 const addButton = document.getElementById('add-contact-btn');
-addButton.addEventListener('click', function(e) {
+addButton.addEventListener('click', function (e) {
   let modal = '';
   modal = `
       <div class="modal-container">
         <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
           <div class="modal-info-container">
-              <form action="#" enctype="multipart/form-data" method="post">
-              <label for="firstname">Name:</label>
-              <input type="text" id="firstname" name="firstname">
+              <form action="#" class="modal-form" enctype="multipart/form-data" method="post">
+              <div>
+              <label class="modal-name cap" for="firstname">Name:</label>
+              <input class="modal-text" type="text" id="firstname" name="firstname">
+              </div>
+              <div>
               <label for="surname">Surname:</label>
               <input type="text" id="surname" name="surname">
+              </div>
+              <div>
               <label for="phone">Phone:</label>
               <input type="text" id="phone" name="phone" placeholder="(108)-805-9730">
+              </div>
+              <div>
               <label for="address">Address:</label>
               <input type="text" id="address" name="address">
+              </div>
+              <div>
               <label for="fileToUpload">Image:</label>
               <input type="file" name="fileToUpload" id="fileToUpload">
-              <button id="add-contact" type="submit">Add Contact</button>
+              </div>
+              <div>
+              <button class="btn" id="add-contact" type="submit">Add Contact</button>
+              </div>
               </form>
           </div>
         </div>
@@ -104,9 +117,17 @@ addButton.addEventListener('click', function(e) {
   overlay.innerHTML = modal;
   document.getElementById('gallery').appendChild(overlay);
 
+  // Close Modal
+  const closeButton = document.getElementById('modal-close-btn');
+  closeButton.addEventListener('click', function (e) {
+    this.parentNode.style.display = 'none';
+    // remove modal
+    document.getElementById('gallery').removeChild(overlay);
+  });
+
   // Submit Form
   btnSubmit = document.getElementById('add-contact');
-  btnSubmit.addEventListener('click', function(e) {
+  btnSubmit.addEventListener('click', function (e) {
     e.preventDefault();
     const filePath = document.getElementById('fileToUpload').value;
     const filename = filePath.substring(filePath.lastIndexOf('\\') + 1);
@@ -120,7 +141,7 @@ addButton.addEventListener('click', function(e) {
 
     contacts.push(newContact);
 
-    // Close Modal
+    // Close Modal after submit
     this.parentNode.style.display = 'none';
     document.getElementById('gallery').removeChild(overlay);
 
