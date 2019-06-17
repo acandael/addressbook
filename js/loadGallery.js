fa4f7561e5ff8233fdd3cfe6a4a04085b05570f2
acandael <info@anthonycandaele.com>
2019-06-17 21:02:07 +0200

    move loadgallery to its own file

---
+++ js/loadGallery.js
@@ -0,0 +1,27 @@
+// Generate Gallery
+const loadGallery = () => {
+  if (contacts.length > 0) {
+    let card = '';
+    contacts.forEach((contact, index) => {
+      card += `
+    <div class="card">
+                  <div class="card-img-container">
+                      <img data-card="${index}" class="card-img" src="${
+        contact.image
+      }" alt="profile picture">
+                  </div>
+                  <div class="card-info-container">
+                      <h3 id="name" class="card-name cap">${contact.name} ${
+        contact.surname
+      }</h3>
+                      <p class="card-text">${contact.phone}</p>
+                      <p class="card-text cap">${contact.address}</p>
+                  </div>
+              </div>
+    `;
+      document.getElementById('gallery').innerHTML = card;
+    });
+  } else {
+    document.getElementById('gallery').innerHTML = '';
+  }
+};
