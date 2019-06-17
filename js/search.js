6ff4bf0ba38c93e5041679e17b0226d45371a52e
acandael <info@anthonycandaele.com>
2019-06-17 20:38:27 +0200

    refactor search

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-var options = {
+const options = {
   shouldSort: false,
   threshold: 0.2,
   location: 0,
@@ -7,20 +7,22 @@ var options = {
   minMatchCharLength: 2,
   keys: ['name', 'surname']
 };
-var fuse = new Fuse(contacts, options);
 
+const fuse = new Fuse(contacts, options);
 const searchSubmit = document.getElementById('search-submit');
 const searchInput = document.getElementById('search-input');
-let input = '';
+
+const search = () => {
+  const input = document.getElementById('search-input').value;
+  contacts = fuse.search(input);
+};
 
 searchInput.addEventListener('keypress', function(e) {
-  input = document.getElementById('search-input').value;
-  contacts = fuse.search(input);
+  search();
   loadGallery();
 });
 
 searchSubmit.addEventListener('click', function(e) {
-  input = document.getElementById('search-input').value;
-  contacts = fuse.search(input);
+  search();
   loadGallery();
 });
